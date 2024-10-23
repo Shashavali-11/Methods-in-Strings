85e233c9a005dd7ded7891d81ec45b4afcc3c9ce
Shashavali <109945362+Shashavali-11@users.noreply.github.com>
2024-10-23 08:36:43 +0530

    Add files via upload
    Task - 4

---
+++ task4.js
@@ -0,0 +1,45 @@
+// 21-10-2024 -- Monday
+
+
+// Join Method
+var fruits = [ 'Apple', 'Banana', 'Orange' ]
+console.log(fruits.join())
+
+var numbers = [1,2,3,4,5]
+console.log(numbers.join("-"))
+
+var chars = ['H', 'e', 'l', 'l', 'o']
+console.log(chars.join(''))
+console.log()
+// --------------------------------------------------------------------------------------------
+
+
+// Slice Method
+var text = 'JavaScript is awesome'
+console.log(text.slice(0,10))
+
+var sentence = 'I love learning JavaScript!'
+console.log(sentence.slice(7,15))
+
+var text1 = 'Frontend Development'
+console.log(text1.slice(-11))
+console.log()
+// --------------------------------------------------------------------------------------------
+
+// Split Method
+var date = '2024-10-21'
+console.log(date.split("-"))
+
+var texts = 'I love JavaScript Programming'
+console.log(texts.split( " ", 2 ))
+
+var x = texts.split(" ")
+console.log(x[3])
+
+var url = "https://fakestoreapi.com/products"
+console.log(url.split("/"))
+
+
+
+
+
